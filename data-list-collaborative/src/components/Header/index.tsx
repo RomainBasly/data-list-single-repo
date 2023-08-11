f0c68ce4fd8ee65042d3190538296210726c9ee5
jlb350 <jeanlucbonneville@gmail.com>
2023-08-11 16:32:03 +0200

    feature-1 elements header

--- data-list-collaborative/src/components/Header/index.tsx
+++ data-list-collaborative/src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import classes from "./classes.module.scss";
-import { Bars3Icon } from '@heroicons/react/24/solid'
-import logo from "../../../public/logo.png";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import logo from "../../../public/list.svg";
 
 export default function Header() {
-    return (
-        <div className={classes["root"]}>
-            <img src={String(logo.src)} alt="" className={classes["logo"]}/>
-            <Bars3Icon className={classes["icon"]} />
-        </div>
-    )
+  return (
+    <div className={classes["root"]}>
+      <img
+        src={String(logo.src)}
+        className={classes["logo"]}
+        alt="Picture of the author"
+      />
+
+      <Bars3Icon className={classes["icon"]} />
+    </div>
+  );
 }
