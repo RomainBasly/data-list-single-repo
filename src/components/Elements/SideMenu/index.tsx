09d3b7553cfa5e0742d2121923d4b38d2d67dcb0
jlb350 <jeanlucbonneville@gmail.com>
2023-08-26 19:08:14 +0200

    sidemenuroot

--- src/components/Elements/SideMenu/index.tsx
+++ src/components/Elements/SideMenu/index.tsx
@@ -1,63 +1,66 @@
-'use client';
+"use client";
 import * as React from "react";
 import { useEffect } from "react";
 import classes from "./classes.module.scss";
-import {XMarkIcon} from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import UserMenuStatus, { EOpeningState } from "@/Stores/UserMenuStatus";
 import { HomeIcon, PencilIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import NavLink from "@/components/Materials/NavLink";
 
 type IProps = {
-    status?: EOpeningState;
-}
+  status?: EOpeningState;
+};
 
 export default function SideMenu(props: IProps) {
-    const [openingState, setOpeningState] = React.useState<EOpeningState>(UserMenuStatus.getInstance().status);
+  const [openingState, setOpeningState] = React.useState<EOpeningState>(
+    UserMenuStatus.getInstance().status
+  );
 
-    useEffect(() => {
-		const removeOnStatusChange = UserMenuStatus.getInstance().onChange(userMenuStatusChangeListener);
-		return () => {
-			removeOnStatusChange();
-		};
-	}, [updateStatus, openingState, props]);
-
-    function userMenuStatusChangeListener() {
-		setOpeningState(UserMenuStatus.getInstance().status);
-	};
-
-    function updateStatus() {
-        UserMenuStatus.getInstance().toggle();
-    }
-
-    function close() {
-        UserMenuStatus.getInstance().toggle();
-    }
-
-    const rootProps = {status: openingState};
-    return (
-      <div className={classes["root"]} {...rootProps}>
-        <XMarkIcon className={classes["closing-svg"]} onClick={close} />
-        <div className={classes["nav-link-container"]}>
-            
-          <NavLink
-            svg={<HomeIcon />}
-            className={classes["nav-link"]}
-            text={"Home"}
-            alt={"Home Icon"}
-          />
-          <NavLink
-            svg={<PencilIcon />}
-            className={classes["nav-link"]}
-            text={"Créer une liste"}
-            alt={"Add a list Icon"}
-          />
-          <NavLink
-            svg={<XCircleIcon />}
-            className={classes["nav-link"]}
-            text={"Effacer une liste"}
-            alt={"Add a list Icon"}
-          />
-        </div>
-      </div>
+  useEffect(() => {
+    const removeOnStatusChange = UserMenuStatus.getInstance().onChange(
+      userMenuStatusChangeListener
     );
+    return () => {
+      removeOnStatusChange();
+    };
+  }, [updateStatus, openingState, props]);
+
+  function userMenuStatusChangeListener() {
+    setOpeningState(UserMenuStatus.getInstance().status);
+  }
+
+  function updateStatus() {
+    UserMenuStatus.getInstance().toggle();
+  }
+
+  function close() {
+    UserMenuStatus.getInstance().toggle();
+  }
+
+  const rootProps = { status: openingState };
+  return (
+    <div className={classes["root"]} {...rootProps}>
+      <XMarkIcon className={classes["closing-svg"]} onClick={close} />
+      <div className={classes["nav-link-container"]}>
+        <NavLink
+          svg={<HomeIcon />}
+          className={classes["nav-link"]}
+          text={"Home"}
+          alt={"Home Icon"}
+        />
+        <NavLink
+          svg={<PencilIcon />}
+          className={classes["nav-link"]}
+          text={"Créer une liste"}
+          alt={"Add a list Icon"}
+        />
+        <NavLink
+          svg={<XCircleIcon />}
+          className={classes["nav-link"]}
+          text={"Effacer une liste"}
+          alt={"Add a list Icon"}
+        />
+      </div>
+    </div>
+  );
 }
