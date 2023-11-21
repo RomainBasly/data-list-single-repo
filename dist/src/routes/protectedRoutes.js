b431ad1d030a10a116f6311dbab9922a6e9c5c22
Romain Basly <romainbasly@MacBook-Pro-de-Romain.local>
2023-11-21 16:48:19 +0100

    bugfix - order of execution of env variables when using jwt

--- dist/src/routes/protectedRoutes.js
+++ dist/src/routes/protectedRoutes.js
@@ -8,5 +8,5 @@ const api_1 = require("../common/types/api");
 const auth_middleware_1 = require("../middlewares/auth-middleware");
 const appUserController = tsyringe_1.container.resolve(controllers_1.AppUserController);
 const protectedRoutes = (0, express_1.Router)();
-protectedRoutes.get("/api/users/all", (0, auth_middleware_1.verifyRoles)(api_1.Roles.ADMIN), (req, res) => appUserController.getAllUsers(req, res));
+protectedRoutes.get("/api/users/all", (0, auth_middleware_1.verifyRoles)(api_1.Roles.ADMIN, api_1.Roles.USER), (req, res) => appUserController.getAllUsers(req, res));
 exports.default = protectedRoutes;
