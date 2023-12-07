269e9cc52b2e9e59590fcea00ece07476ca4f345
Romain Basly <romainbasly@MacBook-Pro-de-Romain.local>
2023-12-07 15:06:42 +0100

    fix the icon on computer

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.png" />
       </Head>
       <div className={classes['root']}>
         <DefaultTemplate></DefaultTemplate>
