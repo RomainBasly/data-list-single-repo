51afc1b53a0c50d3e615faefcd92674ed0538f07
Romain Basly <romain.basly@protonmail.com>
2023-12-10 13:31:19 +0100

    amend the device to restrict drastically the rate limit

--- dist/middlewares/common.js
+++ dist/middlewares/common.js
@@ -5,6 +5,6 @@ const express_rate_limit_1 = require("express-rate-limit");
 exports.limiter = (0, express_rate_limit_1.rateLimit)({
     windowMs: 15 * 60 * 1000,
     limit: 1,
-    standardHeaders: "draft-7",
+    standardHeaders: true,
     legacyHeaders: false,
 });
