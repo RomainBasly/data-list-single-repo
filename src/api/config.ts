8480e87c507d9e15864e805dc72b6c294583b0fe
Romain Basly <romain.basly@protonmail.com>
2023-12-10 07:52:12 +0100

    fix little typo error deploying connection between front and back

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
-export const BACK_URL = "https://stingray-app-69yxe.ondigitalocean.app";
+export const BACK_URL = "https://stingray-app-69yxe.ondigitalocean.app/api";
 //export const BACK_URL = "http://localhost:8000/api";
