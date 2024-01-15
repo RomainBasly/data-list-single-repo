f86f26aae255a57d7352624dbd09f972750b1afc
Romain Basly <romain.basly@protonmail.com>
2024-01-15 11:56:52 +0100

    modify the cookie expiration to 24hours

--- src/Services/CookieService.ts
+++ src/Services/CookieService.ts
@@ -13,7 +13,7 @@ export default class CookieService {
     if (!value || !name) throw new Error("Cookie name or value is empty");
     const date = new Date();
 
-    date.setTime(date.getTime() + 3600 * 1000);
+    date.setTime(date.getTime() + 3600 * 1000 * 24);
 
     document.cookie =
       name + "=" + value + "; expires=" + date.toUTCString() + "path=/";
