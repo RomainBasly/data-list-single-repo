1627b3dfe933238262472e7491c963c25e8b69c7
Romain Basly <romain.basly@protonmail.com>
2024-01-16 20:00:40 +0100

    fix the error I made just now

--- src/api/app-refresh-token/controller.ts
+++ src/api/app-refresh-token/controller.ts
@@ -32,7 +32,7 @@ export class AppRefreshTokenController {
         accessTokenSecret,
         foundUser
       );
-      cookieHandler(req, res, newRefreshToken, '.simplists.net');
+      cookieHandler(req, res, newRefreshToken);
       res.json({ accessToken: newAccessToken });
     } catch (error) {
       next(error);
