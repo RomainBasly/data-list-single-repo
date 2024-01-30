3ca243acd04297d0e523e1032b0a810bc0704cab
Romain Basly <romain.basly@protonmail.com>
2024-01-30 16:49:59 +0100

    refacto middleware to externalize the CSP

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+import CspService from "@/Services/cspServices";
 import JwtService from "@/Services/jwtService";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
@@ -14,20 +15,7 @@ const privatePages = [
 const logPagesArray = ["/login", "/register"];
 
 export default async function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-  const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-  style-src 'self' 'nonce-${nonce}' fonts.googleapis.com;
-  font-src 'self' fonts.gstatic.com;
-  img-src 'self' data:;
-  connect-src 'self' https://stingray-app-69yxe.ondigitalocean.app http://localhost:8000;
-  base-uri 'self';
-  object-src 'none';
-`;
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, " ")
-    .trim();
+  const { nonce, contentSecurityPolicyHeaderValue} = CspService.getInstance().initiateCsp()
 
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
@@ -49,9 +37,6 @@ export default async function middleware(request: NextRequest) {
   );
 
   if (isPostMethod) {
-    if (isLoggedIn && isLogPage) {
-      return NextResponse.redirect(new URL("/profile", request.url));
-    }
     return NextResponse.next();
   }
   if (isGetMethod) {
