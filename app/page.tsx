7e230b7f9eb3b082db0e6e65ffee42cf32fd4c0d
Romain Basly <romain.basly@protonmail.com>
2024-02-04 21:14:27 +0100

    last modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,11 @@
-.root {
-  display: flex;
-  padding: 40px 100px;
-  flex-direction: column;
-  gap: 20px;
-  max-width: 650px;
-  justify-content: center;
-  margin: auto;
+import classes from './classes.module.scss'
+import { Loader } from '@/components/Elements/Loader'
+import { LandingHeader } from '@/components/Elements/Headers/LandingHeader'
+import Layout from '@/components/Elements/Layout'
+import Button from '@/components/Materials/Button'
+import { FolderPlusIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link'
 
-  @media screen and (max-width: 500px) {
-    padding: 20px 60px;
-  }
-
-  .title-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    .title {
-      color: #353535;
-    }
-    svg {
-      width: 20px;
-    }
-  }
-
-  .redirection-button-container {
-    border: 1px solid #dee2e6;
-    padding: 8px;
-    border-radius: 4px;
-    cursor: pointer;
-    background-color: #efe9ae;
-    //background-color: #ffcb77b3;
-
-    transition: border-color 1s, background-color 1s;
-
-    &:hover {
-      border-color: #adb5bd;
-      background-color: #ffcb77b3;
-    }
-    .create-list-button {
-      border: none;
-      cursor: pointer;
-      background-color: inherit;
-
-      svg {
-        max-width: 20px;
-        max-height: 20px;
-        width: 100%;
-        height: 100%;
-      }
-    }
-  }
+export default function Home() {
+  return
 }
