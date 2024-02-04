faa42be350186c4bd317d2952c5912dee6e1ed1d
Romain Basly <romain.basly@protonmail.com>
2024-02-04 21:14:30 +0100

    last modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import classes from './classes.module.scss'
-import { Loader } from '@/components/Elements/Loader'
-import { LandingHeader } from '@/components/Elements/Headers/LandingHeader'
-import Layout from '@/components/Elements/Layout'
-import Button from '@/components/Materials/Button'
-import { FolderPlusIcon } from '@heroicons/react/24/solid'
-import Link from 'next/link'
-
 export default function Home() {
   return
 }
