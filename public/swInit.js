f94906848cfc64f210cac2a188e7f4d2b18bc738
Romain Basly <romain.basly@protonmail.com>
2024-02-21 11:52:40 +0100

    try to debug sw on mobile 1000 attempt

--- public/swInit.js
+++ public/swInit.js
@@ -1,110 +1,99 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js"
 );
-
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+workbox.precaching.precacheAndRoute([
+  { url: "/offline", revision: 29 },
+  { url: "/", revision: 29 },
+]);
 
 // if (workbox) {
-//   workbox.precaching.precacheAndRoute([{ url: "/", revision: "23" }]);
-
-//   workbox.routing.registerRoute(
-//     ({ url, request }) => request.mode === "navigate",
-//     new workbox.strategies.CacheFirst({
-//       cacheName: "pages-cache",
-//       plugins: [
-//         new workbox.cacheableResponse.CacheableResponsePlugin({
-//           statuses: [0, 200],
-//         }),
-//         new workbox.expiration.ExpirationPlugin({
-//           maxEntries: 50,
-//           maxAgeSeconds: 30 * 24 * 60 * 60,
-//         }),
-//       ],
-//     }),
-//     {
-//       ignoreURLParametersMatching: [/^_rsc$/],
-//     }
-//   );
-
-//   workbox.routing.registerRoute(
-//     ({ request }) => request.destination === "font",
-//     new workbox.strategies.CacheFirst({
-//       cacheName: "font-cache",
-//       plugins: [
-//         new workbox.cacheableResponse.CacheableResponsePlugin({
-//           statuses: [0, 200],
-//         }),
-//         new workbox.expiration.ExpirationPlugin({
-//           maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-//           maxEntries: 30,
-//         }),
-//       ],
-//     })
-//   );
-
-//   workbox.routing.registerRoute(
-//     ({ request }) => ["style", "script", "image"].includes(request.destination),
-//     new workbox.strategies.CacheFirst({
-//       cacheName: "assets-cache",
-//       plugins: [
-//         new workbox.expiration.ExpirationPlugin({
-//           maxEntries: 100, // Adjust based on your needs
-//           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//         }),
-//       ],
-//     })
-//   );
-
-//   workbox.routing.registerRoute(
-//     new RegExp("https://stingray-app-69yxe.ondigitalocean.app/.*"),
-//     new workbox.strategies.NetworkFirst({
-//       cacheName: "backend-api-cache",
-//       plugins: [
-//         new workbox.expiration.ExpirationPlugin({
-//           maxEntries: 50,
-//           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//         }),
-//       ],
-//     })
-//   );
-//   workbox.routing.registerRoute(
-//     new RegExp("http://localhost:8000/api/"),
-//     new workbox.strategies.NetworkFirst({
-//       cacheName: "api-cache-dev",
-//       plugins: [
-//         new workbox.expiration.ExpirationPlugin({
-//           maxEntries: 50,
-//           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//         }),
-//       ],
-//     })
-//   );
-//   workbox.routing.setCatchHandler(({ event }) => {
-//     switch (event.request.destination) {
-//       case "document":
-//         return caches.match("/offline");
-//         break;
-//       case "image":
-//         return caches.match("/images/logos/logo-96x96.png");
-//         break;
-//       default:
-//         // If we don't have a fallback, just return an error response.
-//         return Response.error();
-//     }
-//   });
-// }
+workbox.routing.registerRoute(
+  ({ url, request }) => request.mode === "navigate",
+  new workbox.strategies.NetworkFirst({
+    cacheName: "pages-cache",
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+  // {
+  //   ignoreURLParametersMatching: [/^_rsc$/],
+  // }
+);
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "font",
+  new workbox.strategies.CacheFirst({
+    cacheName: "font-cache",
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+workbox.routing.registerRoute(
+  ({ request }) => ["style", "script", "image"].includes(request.destination),
+  new workbox.strategies.CacheFirst({
+    cacheName: "assets-cache",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 100, // Adjust based on your needs
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+// workbox.routing.registerRoute(
+//   new RegExp("https://stingray-app-69yxe.ondigitalocean.app/.*"),
+//   new workbox.strategies.NetworkFirst({
+//     cacheName: "backend-api-cache",
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxEntries: 50,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   })
+// );
+// workbox.routing.registerRoute(
+//   new RegExp("http://localhost:8000/api/"),
+//   new workbox.strategies.NetworkFirst({
+//     cacheName: "api-cache-dev",
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxEntries: 50,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   })
+// );
+workbox.routing.setCatchHandler(({ event }) => {
+  switch (event.request.destination) {
+    case "document":
+      return caches.match("/offline");
+      break;
+    case "image":
+      return caches.match("/images/logos/logo-96x96.png");
+      break;
+    default:
+      // If we don't have a fallback, just return an error response.
+      return Response.error();
+  }
+});
 
 // Use self instead of window for service workers
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open("my-app-cache").then((cache) => {
-      return cache.addAll([
-        "/manifest.json", // Add manifest.json to the list of files to cache
-        // List other assets here
-      ]);
-    })
-  );
   console.log("Service Worker installing.");
 });
 
