9751253ea6b79fd9e285bdcdec95c0df79d7988b
Romain Basly <romain.basly@protonmail.com>
2024-04-28 18:03:52 +0200

    wip

--- src/services/SocketService.ts
+++ src/services/SocketService.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server as IOServer, Socket } from "socket.io";
-import dotenv from "dotenv";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { randomBytes } from "node:crypto";
 
 export class SocketService {
@@ -10,10 +10,8 @@ export class SocketService {
   private io: IOServer;
   private readonly port: string | number;
   private static instance: SocketService;
-  private userSocketMap: Map<
-    string,
-    { socketConnectionId: string; userId: string | null }
-  >;
+  private userSocketMap: Map<string, { socketId: string; userId?: string }>;
+
   constructor() {
     this.expressApp = express();
     this.httpServer = http.createServer();
@@ -25,7 +23,8 @@ export class SocketService {
         credentials: true, // Allow sending of cookies and credentials
       },
     });
-    this.port = process.env.PORT || 3002;
+    this.port = process.env.PORT || 3001;
+
     this.userSocketMap = new Map();
 
     this.initializeSocket();
@@ -41,62 +40,85 @@ export class SocketService {
 
   private initializeSocket(): void {
     this.io.on("connection", (socket: Socket) => {
-      const socketConnectionId = randomBytes(16).toString("hex");
-      socket.emit("assign-id", {
-        socketConnectionId: socketConnectionId,
-        userId: null,
+      const socketId = randomBytes(16).toString("hex");
+      socket.emit("assign-socket-id", {
+        socketId,
       });
-      this.userSocketMap.set(socketConnectionId, {
-        socketConnectionId: socket.id,
-        userId: null,
+      this.userSocketMap.set(socketId, {
+        socketId,
       });
+      console.log("this.userSocketMap", this.userSocketMap);
       socket.on("ping", () => {
         socket.emit("pong");
       });
       socket.on("disconnect", () => {
-        this.userSocketMap.delete(socketConnectionId);
-        console.log("client disconnected");
+        this.userSocketMap.delete(socketId);
+        this.userSocketMap.delete(socket.id);
+        // const userId = [...this.userSocketMap.entries()].find(
+        //   ([_, value]) => value.socketId === socket.id
+        // )?.[0];
+        // if (userId) {
+        //   this.userSocketMap.delete(userId);
+        //   console.log(`User ${userId} disconnected, removing entry from map.`);
+        // }
+        console.log("disconnect", this.userSocketMap, socket.id);
       });
 
       socket.on("list-invitation-backend", (data: any) => {
-        const { userId, listId } = data;
-        console.log("data", data);
-        console.log("this.userSocketMap", this.userSocketMap.entries());
+        const { userId } = data;
 
         let targetSocketId: string | undefined;
 
         // Iterate over the map to find the matching userId
         for (const [key, value] of this.userSocketMap.entries()) {
           if (String(value.userId) === String(userId)) {
-            targetSocketId = value.socketConnectionId;
+            targetSocketId = value.socketId;
             break; // Stop searching once we've found the userId
           }
         }
 
-        console.log("targetSocketId", targetSocketId);
-
         if (targetSocketId) {
-          this.io
-            .to(targetSocketId)
-            .emit("list-invitation-socket", { userId, listId });
+          console.log("data", data);
+          this.io.to(targetSocketId).emit("list-invitation-socket", {
+            data,
+          });
         }
       });
 
       socket.on("register-user-id", (data) => {
-        const { userId, socketConnectionId } = data;
+        const { accessTokenJWT, socketId } = data;
 
-        Array.from(this.userSocketMap.entries()).forEach(([key, value]) => {
-          if (value.userId === userId) {
-            this.userSocketMap.delete(key); // Remove old entry
-          }
-        });
+        const decoded = jwt.decode(accessTokenJWT);
+        if (
+          typeof decoded === "object" &&
+          decoded !== null &&
+          "userInfo" in decoded
+        ) {
+          const userId = decoded.userInfo.id.toString();
+          // Array.from(this.userSocketMap.entries()).forEach(([key, value]) => {
+          //   if (value.userId === userId) {
+          //     this.userSocketMap.delete(key); // Remove old entry to prevent many
+          //   }
+          // });
 
-        if (this.userSocketMap.has(socketConnectionId)) {
-          this.userSocketMap.set(socketConnectionId, {
-            socketConnectionId: socket.id,
-            userId,
+          this.userSocketMap.forEach((value, key) => {
+            if (value.userId === userId) {
+              this.userSocketMap.delete(key);
+            }
           });
+
+          if (this.userSocketMap.has(socketId)) {
+            this.userSocketMap.set(socketId, {
+              socketId: socket.id,
+              userId,
+            });
+          }
+          console.log(
+            `Registering or updating userId ${userId} with socketId ${socket.id}`
+          );
         }
+
+        console.log("this.userSocketId", this.userSocketMap);
       });
     });
   }
