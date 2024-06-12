adfc1506374f30c7f93397211cb559154d19989d
Romain Basly <romain.basly@protonmail.com>
2024-06-12 19:27:37 +0200

    modify the origins for the development mode

--- src/services/SocketService.ts
+++ src/services/SocketService.ts
@@ -17,10 +17,7 @@ export class SocketService {
     this.httpServer = http.createServer();
     this.io = new IOServer(this.httpServer, {
       cors: {
-        origin: [
-          "https://data-list-collaborative-r54h7zfc9-romainbaslys-projects.vercel.app/",
-          "https://stingray-app-69yxe.ondigitalocean.app/api",
-        ], // Allowed origins
+        origin: ["localhost:3000", "localhost:8000"], // Allowed origins
         methods: ["GET", "POST"], // Allowed HTTP request methods
         allowedHeaders: ["my-custom-header"], // Custom headers that can be sent
         credentials: true, // Allow sending of cookies and credentials
