1ef7161a5e34acb9460df96c7b607486d5561d01
Romain Basly <romain.basly@protonmail.com>
2024-06-26 16:05:37 +0200

    bugfix csp for the fonts

--- src/Services/cspServices.ts
+++ src/Services/cspServices.ts
@@ -18,7 +18,7 @@ export default class CspService {
   style-src 'self' 'nonce-${nonce}' fonts.googleapis.com;
   font-src 'self' fonts.gstatic.com;
   img-src 'self' data:;
-  connect-src 'self' https://stingray-app-69yxe.ondigitalocean.app http://localhost:8000/api/ wss://ws.simplists.net https://fonts.googleapis.com;
+  connect-src 'self' https://stingray-app-69yxe.ondigitalocean.app http://localhost:8000/api/ wss://ws.simplists.net https://fonts.googleapis.com https://fonts.gstatic.com;
   base-uri 'self';
   object-src 'none';
 `;
